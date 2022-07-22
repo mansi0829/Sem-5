d8b848a8cb1b89a58dd417f2ef37e9b991ac1983
Mansi Gundre <81025790+mansi0829@users.noreply.github.com>
2022-07-22 12:50:37 +0530

    sort for arrival time added

--- LP-1/19-7-22/FirstComeFirstServe.cpp
+++ LP-1/19-7-22/FirstComeFirstServe.cpp
@@ -1,7 +1,13 @@
+//============================================================================
+// Name        : a131.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+
 #include <bits/stdc++.h>
-#include <cctype>
-#include <iostream>
-#include <cstring>
 typedef long long int ll;
 using namespace std;
 
@@ -10,9 +16,46 @@ class calculate
 public:
     void createProcess();
     void printProcess(int at[], int bt[], int tat[], int wt[], int n);
+//    void processSequence(int at[], int bt[], int n);
     void calculateAvgWaitingTime(int at[], int bt[], int wt[], int n);
     void calculateAvgTurnAroundTime(int at[], int bt[], int tat[], int n);
 };
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void sort(int at[],int bt[], int n)
+{
+	int min_idx;
+	for (int i = 0; i < n-1; i++)
+	    {
+	        min_idx = i;
+	        for (int j = i+1; j < n; j++)
+	        if (at[j] < at[min_idx])
+	       		min_idx = j;
+	        swap(&at[min_idx], &at[i]);
+	        swap(&bt[min_idx], &bt[i]);
+	    }
+}
+
+void printArray(int at[],int n)
+{
+	for(int i=0;i<n;i++) cout<<at[i]<<" ";
+}
+
+void processSequence(int at[],int bt[], int n)
+{
+	for(int i=0;i<n;i++){
+		if(at[i]>at[i+1]){
+			swap(&at[i] , &at[i+1]);
+			swap(&bt[i] , &bt[i+1]);
+		}
+	}
+}
 
 void calculate::createProcess()
 {
@@ -27,15 +70,19 @@ void calculate::createProcess()
 
 void calculate::printProcess(int at[], int bt[], int tat[], int wt[], int n)
 {
-    tat[0] = bt[0];
-    cout << "PN\t\tAT\t\t"
-         << "BT\t\tTAT\t\t"
-         << "WT\n\n";
+	sort(at,bt, n);
+//	void calculate::
+
+//	processSequence(at,bt,n);
+	tat[0] = bt[0];
+    cout<<"PN\t\tAT\t\t"
+         << "BT\t\tTAT\t\tWT";
+
     cout << "1"
          << "\t\t" << at[0] << "\t\t"
          << bt[0] << "\t\t" << tat[0] << "\t\t" << wt[0] << endl;
 
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         wt[i] = (at[i - 1] + bt[i - 1] + wt[i - 1]) - at[i];
         tat[i] = bt[i] + wt[i];
@@ -44,6 +91,10 @@ void calculate::printProcess(int at[], int bt[], int tat[], int wt[], int n)
              << tat[i] << "\t\t" << wt[i] << endl;
     }
 }
+
+
+
+
 
 void calculate::calculateAvgWaitingTime(int at[], int bt[], int wt[], int n)
 {
@@ -60,6 +111,7 @@ void calculate::calculateAvgWaitingTime(int at[], int bt[], int wt[], int n)
     cout << "\nAverage waiting time = "
          << average;
 }
+
 void calculate::calculateAvgTurnAroundTime(int at[], int bt[], int tat[], int n)
 {
     float average;
@@ -86,7 +138,7 @@ int main()
         cin >> at[i] >> bt[i];
     int wt[n], tat[n];
     wt[0] = 0;
-
+//    sort(at,n);
     obj.printProcess(at, bt, tat, wt, n);
     obj.calculateAvgWaitingTime(at, bt, wt, n);
     obj.calculateAvgWaitingTime(at, bt, tat, n);
